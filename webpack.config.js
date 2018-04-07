fc9cd11df46b3204fe12d05047ee403e373991dc
ut-ymmt <ut08.ymmt11@gmail.com>
2018-04-07 18:50:21 +0900

    複数ファイルをbundleできるよう変更

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,20 @@
-const path = require('path');
+let path = require('path');
+let glob = require("glob");
+
+const jsBasePath = path.resolve(__dirname, 'src/');
+const jsCompileFolders = ['js'];
+const targets = glob.sync(`${jsBasePath}/+(${jsCompileFolders.join('|')})/*.js`);
+
+const entries = {};
+targets.forEach(value => {
+  const re = new RegExp(`${jsBasePath}/`);
+  const key = value.replace(re, '');
+  entries[key] = value;
+});
+
 
 module.exports = {
-  entry: './src/js/index.js',
+  entry: entries,
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist/js')
